ba0ae8fd9e40c527ba3a134d0725adf144022e55
zhuyifei.ruichuang <zhuyifei.ruichuang@gmail.com>
2025-09-03 14:18:37 +0800

    Update mc_get.sh

--- minio/mc_get.sh
+++ minio/mc_get.sh
@@ -2,6 +2,6 @@
 # 用于拉取minio对象存储指定文件 
 
 # 下载指定文件到指定目录
+mc get myminio/kubesphere/configFile/deploy_v6.yaml /opt/
 mc get myminio/kubesphere/kubesphere/kubekey/kubekey-v3.1.11-linux-amd64.tar.gz /opt/
 mc get myminio/kubesphere/artifact/ks341offline_v5.tar.gz /opt/
-mc get myminio/kubesphere/configFile/deploy_v6.yaml /opt/
