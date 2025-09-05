6b2b555fb56e6c63863aa1439768faf1f259376b
zhuyifei.ruichuang <zhuyifei.ruichuang@gmail.com>
2025-09-05 11:14:03 +0800

    Update download_packages.sh
    增加了目录权限处理，解决以下问题
    “W: Download is performed unsandboxed as root as file '/opt/yq/cdebconf_0.271ubuntu3_amd64.deb' couldn't be accessed by user '_apt'. - pkgAcquire::Run (13: Permission denied)”

--- ubuntu/download_packages.sh
+++ ubuntu/download_packages.sh
@@ -37,6 +37,14 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
+# 兼容处理：不依赖_apt组，直接放宽目录权限（允许所有用户读写）
+# 解决"invalid group: '_apt:_apt'"错误
+sudo chmod -R 777 "$download_dir"
+if [ $? -ne 0 ]; then
+  echo "无法设置目录 $download_dir 权限"
+  exit 1
+fi
+
 # 切换到下载目录
 cd "$download_dir" || {
   echo "无法进入目录 $download_dir"
